834fb34581a91cffa925316b514d5cf36dc70b22
rion-alphonso <rionalphonso4@gmail.com>
2025-07-04 23:49:10 +0530

    Updated app.java

--- src/main/java/com/devops/lab/App.java
+++ src/main/java/com/devops/lab/App.java
@@ -8,5 +8,6 @@ public class App
 	System.out.println("DevOps Lab 4 completes if the first Line of Output shows Started by SCM");
 	System.out.println("Hello From CI");
 	System.out.println(2 + 2);
+	System.out.println(2 + 5);
     }
 }
